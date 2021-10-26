783218acf56f0421551360735011c47767970076
dernasherbrezon <rodionovamp@mail.ru>
2021-10-26 10:53:57 +0100

    match output to volk-based test

--- test/perf_fir_filter.c
+++ test/perf_fir_filter.c
@@ -27,21 +27,22 @@ int main(void) {
     }
     for (size_t i = 0; i < input_len; i++) {
         // don't care about the loss of data
-        input[i] = i * 2 / 128.0F + I * ((i * 2 + 1) / 128.0F);
+        int8_t cur_index = (int8_t) (i * 2);
+        input[i] = cur_index / 128.0F + I * ((cur_index + 1) / 128.0F);
     }
     double totalTime = 0.0;
-    int total = 10;
+    int total = 1;
     for (int j = 0; j < total; j++) {
-        int total_executions = 10;
+        int total_executions = 1;
         clock_t begin = clock();
         for (int i = 0; i < total_executions; i++) {
             float complex *output = NULL;
             size_t output_len = 0;
             fir_filter_process(input, input_len, &output, &output_len, filter);
-//            for (int i = 0; i < 20; i++) {
-//                printf("%.9f, %.9f ", crealf(output[i]), cimagf(output[i]));
-//            }
-//            printf("\n");
+            for (int i = 0; i < 20; i++) {
+                printf("%.9f, %.9f ", crealf(output[i]), cimagf(output[i]));
+            }
+            printf("\n");
         }
         clock_t end = clock();
         double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
